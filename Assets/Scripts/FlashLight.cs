b3ee9acebe8ef7c4660f4a18ce70720a40e6d7af
NurFajriF <muhammadnurfajri889@gmail.com>
2025-01-10 02:58:31 +0700

    Add FlashLight and PowerUpController scripts

---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    public Light flashlight;
+    public KeyCode toggleKey = KeyCode.F;
+
+    private bool isOn = true;
+
+    void Start()
+    {
+        if (flashlight == null)
+        {
+            Debug.LogError("Flashlight not assigned!");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(toggleKey))
+        {
+            isOn = !isOn;
+            flashlight.enabled = isOn;
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/PowerUpController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpController : MonoBehaviour
+{
+    public float speedBoost = 1.5f; 
+    public float duration = 5f; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
+
+            if (playerMovement != null)
+            {
+                StartCoroutine(ApplySpeedBoost(playerMovement));
+            }
+
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator ApplySpeedBoost(PlayerMovement playerMovement)
+    {
+        float originalWalkSpeed = playerMovement.walkSpeed;
+        float originalSprintSpeed = playerMovement.sprintSpeed;
+
+        playerMovement.walkSpeed *= speedBoost;
+        playerMovement.sprintSpeed *= speedBoost;
+
+        yield return new WaitForSeconds(duration);
+
+        playerMovement.walkSpeed = originalWalkSpeed;
+        playerMovement.sprintSpeed = originalSprintSpeed;
+    }
+}
+
